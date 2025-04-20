34e4a767935fb73868ca2b13f60ddf18759072cb
Baby Yaw Lwi <baby.y.lwi@frontiir.net>
2025-04-20 14:53:27 +0630

    send pong response to ping

--- codecrafters-redis-python/app/main.py
+++ codecrafters-redis-python/app/main.py
@@ -8,7 +8,11 @@ def main():
     # Uncomment this to pass the first stage
     #
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    server_socket.accept() # wait for client; blocks until a client connects 
+    # server_socket.accept() # wait for client; blocks until a client connects 
+
+    # task 2 
+    conn, _ = server_socket.accept()
+    conn.sendall(b"+PONG\r\n")
 
 
 if __name__ == "__main__":
