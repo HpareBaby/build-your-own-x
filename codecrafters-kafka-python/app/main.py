0ba3414bf228d3c80dbe274aab7017539a7f6b0c
Baby Yaw Lwi <baby.y.lwi@frontiir.net>
2025-04-23 09:51:10 +0630

    get correlation_id from request message and send response

--- codecrafters-kafka-python/app/main.py
+++ codecrafters-kafka-python/app/main.py
@@ -9,15 +9,33 @@ def main():
     # Uncomment this to pass the first stage
     #
     server = socket.create_server(("localhost", 9092), reuse_port=True)
+    # Task 2
     # server.accept() # wait for client
+    # server_socket, _ = server.accept()
+    # # server_socket.sendall(b"7")
+    # while server_socket.recv(1024):
+    #     data = struct.pack(">II", 0, 7) # b'\x00\x00\x00\x00\x00\x00\x00\x07'
+    #     # ">" = big-endian (network byte order); the most significant byte goes first 
+    #     # "I" = unsigned 32-bit integer; 4 bytes; 
+    #     # "II" = packing 2 integers back-to-back
+    #     print(data.hex()) # 0000000000000007
+    #     server_socket.sendall(data)
+
+    # Task 3
+    # request => $ echo -n "00000023001200046f7fc66100096b61666b612d636c69000a6b61666b612d636c6904302e3100" | xxd -r -p | nc localhost 9092 | hexdump -C
+    # response => 00 00 00 00  // message_size:   0 (any value works)
+    #             6f 7f c6 61  // correlation_id: 1870644833
     server_socket, _ = server.accept()
-    # server_socket.sendall(b"7")
-    while server_socket.recv(1024):
-        data = struct.pack(">II", 0, 7) # b'\x00\x00\x00\x00\x00\x00\x00\x07'
-        # ">" = big-endian (network byte order); the most significant byte goes first 
-        # "I" = unsigned 32-bit integer; 4 bytes; 
-        # "II" = packing 2 integers back-to-back
-        print(data.hex()) # 0000000000000007
+    while raw_request := server_socket.recv(1024):
+        correlation_id = raw_request[8:12]
+        print(f"correlation_id: {correlation_id}")
+        correlation_id = int.from_bytes(correlation_id, byteorder="big")
+        print(f"correlation_id: {correlation_id}")
+        
+        # print(f"raw_request: {raw_request};") # decoded_request: {raw_request.decode()}; raw_request_hex: {raw_request.hex()}; correlation_id: {correlation_id}")
+        print(f"raw_request: {raw_request}; raw_request_hex: {raw_request.hex()}; correlation_id: {correlation_id}")
+        data = struct.pack(">II", 0, correlation_id)
+        print(f"data: {data}")
         server_socket.sendall(data)
 
 
